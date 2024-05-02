d5fa1a0a0224093dec11c259501d60a5bbb89d6f
Parker <pp101socials@gmail.com>
2024-05-02 08:14:06 -0400

    Fixed

--- pages/api/getPortfolioProjects.js
+++ pages/api/getPortfolioProjects.js
@@ -6,6 +6,18 @@ const upload = multer({
   storage: multer.memoryStorage(),
 });
 
+async function connectToDatabase() {
+  const client = new MongoClient(
+    process.env.PORTFOLIO_PROJECTS_DB_CONNECTION_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+  await client.connect();
+  return client.db("portfolio-projects").collection("projects");
+}
+
 export const config = {
   api: {
     bodyParser: false, // Disables automatic parsing of the request body
@@ -14,154 +26,88 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  let client = null; // Declare client variable outside try-catch block
+  if (req.method === "POST") {
+    // Use multer middleware to handle file upload
+    upload.single("file")(req, res, async (err) => {
+      if (err) {
+        console.error("Error uploading file:", err);
+        res.status(500).json({ error: "Failed to upload file" });
+        return;
+      }
 
-  try {
-    if (req.method === "POST") {
-      // Use multer middleware to handle file upload
-      upload.single("file")(req, res, async (err) => {
-        if (err) {
-          console.error("Error uploading file:", err);
-          res.status(500).json({ error: "Failed to upload file" });
-          return;
-        }
+      const {
+        itemID,
+        projectName,
+        clientName,
+        creationDate,
+        demoLink,
+        description,
+        review,
+        // tools,
+        // achievements,
+      } = req.body;
+      const file = req.file;
 
-        const {
+      if (!file) {
+        console.error("No file uploaded");
+        res.status(400).json({ error: "No file uploaded" });
+        return;
+      }
+
+      const collection = await connectToDatabase();
+
+      const type = "image";
+
+      try {
+        const src = `data:${type};base64,${file.buffer.toString("base64")}`; // Convert file buffer to base64
+
+        await collection.insertOne({
           itemID,
           projectName,
           clientName,
+          src,
           creationDate,
           demoLink,
           description,
           review,
-        } = req.body;
-        const file = req.file;
+          // tools,
+          // achievements,
+        });
 
-        if (!file) {
-          console.error("No file uploaded");
-          res.status(400).json({ error: "No file uploaded" });
-          return;
-        }
-
-        try {
-          client = new MongoClient(
-            process.env.PORTFOLIO_PROJECTS_DB_CONNECTION_URI,
-            {
-              useNewUrlParser: true,
-              useUnifiedTopology: true,
-            }
-          );
-
-          await client.connect();
-          const db = client.db(); // Get the default database
-          const collection = db.collection("projects");
-
-          const type = "image";
-
-          const src = `data:${type};base64,${file.buffer.toString("base64")}`;
-          await collection.insertOne({
-            itemID,
-            projectName,
-            clientName,
-            src,
-            creationDate,
-            demoLink,
-            description,
-            review,
-          });
-
-          res
-            .status(200)
-            .json({ message: "Portfolio project submitted successfully!" });
-        } catch (error) {
-          console.error("Error saving Portfolio project to database: ", error);
-          res.status(500).json({ error: "Failed to save Portfolio project." });
-        }
-        // finally {
-        //   // Close the MongoDB client connection
-        //   if (client) {
-        //     await client.close();
-        //     console.log("CLOSED connection to Portfolio DB");
-        //   }
-        // }
+        res
+          .status(200)
+          .json({ message: "Portfolio project submitted successfully!" });
+      } catch (error) {
+        console.error("Error saving Portfolio project to database: ", error);
+        res.status(500).json({ error: "Failed to save Portfolio project." });
+      }
+    });
+  } else if (req.method === "DELETE") {
+    const { itemID } = req.query;
+    if (!itemID) {
+      return res.status(400).json({ error: "itemID parameter is required" });
+    }
+    const collection = await connectToDatabase();
+    const result = await collection.deleteOne({ itemID: itemID });
+    if (result.deletedCount === 1) {
+      const portfolioProjects = await collection.find().toArray();
+      return res.status(200).json({
+        portfolioProjects,
+        message: "Media item deleted successfully!",
       });
-    } else if (req.method === "DELETE") {
-      const { itemID } = req.query;
-      if (!itemID) {
-        return res.status(400).json({ error: "itemID parameter is required" });
-      }
-      try {
-        client = new MongoClient(
-          process.env.PORTFOLIO_PROJECTS_DB_CONNECTION_URI,
-          {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          }
-        );
-
-        await client.connect();
-        const db = client.db(); // Get the default database
-        const collection = db.collection("projects");
-
-        const result = await collection.deleteOne({ itemID: itemID });
-        if (result.deletedCount === 1) {
-          return res
-            .status(200)
-            .json({ message: "Portfolio project deleted successfully!" });
-        } else {
-          return res.status(404).json({ error: "Portfolio project not found" });
-        }
-      } catch (error) {
-        console.error("Error deleting Portfolio project:", error);
-        return res
-          .status(500)
-          .json({ error: "Failed to delete Portfolio project" });
-      }
-      // finally {
-      //   // Close the MongoDB client connection
-      //   if (client) {
-      //     await client.close();
-      //     console.log("CLOSED connection to Portfolio DB");
-      //   }
-      // }
-    } else if (req.method === "GET") {
-      try {
-        client = new MongoClient(
-          process.env.PORTFOLIO_PROJECTS_DB_CONNECTION_URI,
-          {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          }
-        );
-
-        await client.connect();
-        const db = client.db(); // Get the default database
-        const collection = db.collection("projects");
-
-        const portfolioProjects = await collection.find().toArray();
-        res.status(200).json(portfolioProjects);
-      } catch (error) {
-        console.error("Error retrieving media items from database:", error);
-        res.status(500).json({ error: "Failed to retrieve media items" });
-      }
-      // finally {
-      //   // Close the MongoDB client connection
-      //   if (client) {
-      //     await client.close();
-      //     console.log("CLOSED connection to Portfolio DB");
-      //   }
-      // }
     } else {
-      res.status(405).json({ error: "Method Not Allowed" });
+      return res.status(404).json({ error: "Media item not found.." });
     }
-  } catch (error) {
-    console.error("Error handling request:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  } finally {
-    // Close the MongoDB client connection
-    if (client) {
-      await client.close();
-      console.log("CLOSED connection to Portfolio DB");
+  } else if (req.method === "GET") {
+    try {
+      const collection = await connectToDatabase();
+      const portfolioProjects = await collection.find().toArray();
+      res.status(200).json(portfolioProjects);
+    } catch (error) {
+      console.error("Error retrieving media items from database:", error);
+      res.status(500).json({ error: "Failed to retrieve media items" });
     }
+  } else {
+    res.status(405).json({ error: "Method Not Allowed" });
   }
 }
